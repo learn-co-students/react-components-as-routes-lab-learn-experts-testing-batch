a5baa8c5e43a04860163f6eedc242bb709e22c8d
pajamaw <pjpeterww@gmail.com>
2017-02-20 16:30:11 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index)=>{
+        return(
+          <div key={index}>
+          <h4>{actor.name}</h4>
+          {actor.movies.map((movie)=>{
+            return <p>{movie}</p>
+          })}
+          </div>
+        )
+      })}
+    </div>
+  );
 };
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
-  return (<div />);
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index)=>{
+        return(
+          <div key={index}>
+          <h4>{director.name}</h4>
+          {director.movies.map((movie)=>{
+            return <p>{movie}</p>
+          })}
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Directors
